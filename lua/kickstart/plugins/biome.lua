2f17eeb8dd9ccec2f29f2160fe4104ccdce28908
evaries <nadtochii.e@gmail.com>
2024-07-22 17:18:32 +0800

    updated config

---
+++ lua/kickstart/plugins/biome.lua
@@ -0,0 +1,8 @@
+return {
+    -- default_config = {
+    --   cmd = { 'biome', 'lsp-proxy' },
+    --   filetypes = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescript.tsx", "typescriptreact", "astro", "svelte", "vue", "css" },
+    -- --   root_dir = root_pattern('biome.json', 'biome.jsonc'),
+    --   single_file_support = false,
+    -- },
+  }
